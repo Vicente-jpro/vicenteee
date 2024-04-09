c2542b9524504aa3e965f81701acaef27ff7d514
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-04-09 20:47:10 +0100

    File persistence added

---
+++ src/main/java/vicenteee/model/Contacto.java
@@ -0,0 +1,13 @@
+package vicenteee.model;
+
+import lombok.Data;
+
+@Data
+public class Contacto {
+
+	private Long id;
+	private String email;
+	private String favorito;
+	private String name;
+
+}
