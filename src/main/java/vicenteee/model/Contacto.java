dcb5808958e8cfc0cf4369e0faf264e9b72d653b
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-04-09 21:00:34 +0100

    Contacto model added

--- src/main/java/vicenteee/model/Contacto.java
+++ src/main/java/vicenteee/model/Contacto.java
@@ -1,13 +1,39 @@
 package vicenteee.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Entity;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table( name = "contactos")
 public class Contacto {
-
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column( name = "email")
 	private String email;
+	
+	@Column( name = "favorito")
 	private String favorito;
+	
+	@Column( name = "name")
 	private String name;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataRegistro; 
 
 }
